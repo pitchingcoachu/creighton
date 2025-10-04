fb42f86e1aebb3cf66a3bcfb7a94b62ff271ad75
pitchingcoachu <jgaynor@pitchingcoachu.com>
2025-10-04 12:41:47 -0700

    Change app name from 'vmibaseball' to 'creightonbaseball'

--- deploy_script.R
+++ deploy_script.R
@@ -58,7 +58,7 @@ deploy_app <- function() {
     cat("Deploying to shinyapps.io...\n")
     deployApp(
       appDir = ".",
-      appName = "vmibaseball",
+      appName = "creightonbaseball",
       forceUpdate = TRUE,
       launch.browser = FALSE,
       logLevel = "verbose"
