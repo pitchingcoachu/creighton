37f4f77bf2beacd633447ac3c2257a9b49c3a3f0
pitchingcoachu <jgaynor@pitchingcoachu.com>
2025-10-08 09:35:05 -0700

    Rename app in deployment script

--- deploy_script.R
+++ deploy_script.R
@@ -58,7 +58,7 @@ deploy_app <- function() {
     cat("Deploying to shinyapps.io...\n")
     deployApp(
       appDir = ".",
-      appName = "creightonbaseball",
+      appName = "creightonbaseball1",
       forceUpdate = TRUE,
       launch.browser = FALSE,
       logLevel = "verbose"
